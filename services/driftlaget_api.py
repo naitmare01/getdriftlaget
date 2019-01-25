b1249f17fc820f27517756c68d06571fcd32b17d
David Berndtsson <david.berndtsson@gmail.com>
2019-01-25 11:27:04 +0100

    Added timeout to requests (#4)

--- services/driftlaget_api.py
+++ services/driftlaget_api.py
@@ -5,7 +5,7 @@ def get_published_messages(url):
     querystring = {"state":"published", "limit":"20", "order":"asc"}
 
     try:
-        response = requests.request("GET", url, params=querystring)
+        response = requests.request("GET", url, params=querystring, timeout=10)
         response = response.json()
         return response
     except requests.exceptions.RequestException:
